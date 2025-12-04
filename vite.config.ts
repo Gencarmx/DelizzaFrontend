fd1508bfd8a684c0b0104693575b95ef38e11f03
GowZenitAC <jes_alpuche@outlook.es>
2025-12-04 11:07:43 -0600

    feat: :sparkles: add correct icons to project and change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -22,18 +22,18 @@ export default defineConfig({
       start_url: '/',
       icons: [
         {
-          src: 'pwa-64x64.png',
+          src: 'dlizza-64x64.png',
           sizes: '64x64',
           type: 'image/png'
         }, 
         {
-          src: 'pwa-192x192.png',
+          src: 'dlizza-192x192.png',
           sizes: '192x192',
           type: 'image/png',
           purpose: 'any'
         }, 
         {
-          src: 'pwa-512x512.png',
+          src: 'dlizza-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any'
