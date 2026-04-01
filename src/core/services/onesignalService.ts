bc052451951e4ae083ed373ea42c82f222749577
GowZenitAC <jes_alpuche@outlook.es>
2026-04-01 12:51:54 -0600

    cambios pequeños

--- src/core/services/onesignalService.ts
+++ src/core/services/onesignalService.ts
@@ -10,7 +10,7 @@ export async function initOneSignal(): Promise<void> {
     appId: import.meta.env.VITE_ONESIGNAL_APP_ID_DEV as string,
     safari_web_id: "web.onesignal.auto.166712e5-2aee-41ea-b95e-8df3d8ab1f28",
     serviceWorkerPath: 'push/onesignal/OneSignalSDKWorker.js',
-    serviceWorkerParam: { scope: '/push/onesignal/' },
+    serviceWorkerParam: { scope: '/' },
     promptOptions: {
       slidedown: {
         prompts: [
